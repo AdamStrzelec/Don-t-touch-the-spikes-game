ee9f2b7b825732a46ee0e1e8f98b6abca14a3d37
adastr96 <adam19960@op.pl>
2020-12-07 22:55:34 +0100

    setup project

---
+++ webpack.config.js
@@ -0,0 +1,58 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+    entry: "./src/index.js",
+    output: {
+        filename: "bundle.min.js",
+        path: path.resolve(__dirname, "./dist")
+    },
+    watch: false,
+    mode: "development",
+    devtool: "source-map",
+    module: {
+        rules: [
+            {
+                test: /\.m?js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: ["@babel/preset-env"]
+                    }
+                }
+            },
+            {
+                test: /\.css$/i,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ],
+            },
+            {
+                test: /\.(png|jpe?g|gif|webp|awif|svg)$/i,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: { //grafiki chcemy w katalogu dist/images
+                            context: 'public',
+                            name: '/images/[name]-[hash].[ext]',
+                            publicPath: '/',
+                        },
+                    },
+                ],
+            },
+        ]
+        
+    },
+    
+    plugins: [
+        new MiniCssExtractPlugin(),
+        new HtmlWebpackPlugin({
+            title: "My App",
+            filename: "index.html",
+            template: "src/index.html"
+        })
+    ],
+}

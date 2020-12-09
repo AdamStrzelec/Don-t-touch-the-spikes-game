5f40317b2b33feac18ef36ae447fffc571f43b9f
adastr96 <adam19960@op.pl>
2020-12-09 13:19:06 +0100

    prepare canvas and add infinite loop

--- src/index.js
+++ src/index.js
@@ -1,2 +1,45 @@
 import './main.css';
 
+const canvas = document.querySelector("#canvas");
+const ctx = canvas.getContext("2d");
+
+
+class AnimationFrame {
+    constructor( fps = 60, animate ) {
+        this.requestID = 0;
+        this.fps = fps;
+        this.animate = animate;
+    }
+
+    start() {
+        let then = performance.now();
+        const interval = 1000 / this.fps;
+        const tolerance = 0.1;
+
+        const animateLoop = now => {
+            this.requestID = requestAnimationFrame( animateLoop );
+            const delta = now - then;
+
+            if ( delta >= interval - tolerance ) {
+                then = now - ( delta % interval );
+                this.animate( delta );
+            }
+        };
+        this.requestID = requestAnimationFrame( animateLoop );
+    }
+
+    stop() {
+        cancelAnimationFrame( this.requestID );
+    }
+
+}
+
+
+function draw(){
+
+}
+
+const animLoop = new AnimationFrame(60, draw);
+
+animLoop.start();
+
